3df61f53c4e975bf5f3ca84b40e28d687b26d8b5
Samarth Ghulyani <g.samarth@outlook.com>
2020-07-15 15:55:56 +0530

    Complete Project

---
+++ index.js
@@ -0,0 +1,67 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+      charCheck(this.innerHTML);
+      buttonAnimation(this.innerHTML);
+  });
+
+}
+
+document.addEventListener("keydown", function(event){
+  //console.log(event.key); //this pe pura doc call hoga
+  charCheck(event.key);
+  buttonAnimation(event.key);
+});
+
+function charCheck(key){
+  switch (key) {
+    case "w": //will also work for char(single inv comma)
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+    break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+    break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+    break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+    break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+    break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+    break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+    break;
+
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey){
+
+  document.querySelector("."+currentKey).classList.toggle("pressed");
+  setTimeout(function(){
+    document.querySelector("."+currentKey).classList.toggle("pressed")}
+    , 100);
+
+}
